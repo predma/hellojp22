12493b7b302d85dabe2c9cd2c13d09882b1c891f
Boris Predmerski <Boris Predmerskcls>
2020-05-13 20:20:39 +0200

    Kreirana tablica galerija

--- galerijaslika.sql
+++ galerijaslika.sql
@@ -1,3 +1,11 @@
 drop database if exists galerija;
 create database galerija;
 use galerija;
+
+create table galerija(
+    sifra       int not null primary key auto_increment,
+    naziv       varchar(50) not null,
+    opis        text,
+    lokacija    varchar(50) not null,
+    datum datetime
+);
