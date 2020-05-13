55c0c1f258374b6e2b78a13f9b322433fc1f7d3b
Boris Predmerski <Boris Predmerskcls>
2020-05-13 20:41:19 +0200

    Test unešen foregin key i izvršen insert

--- galerijaslika.sql
+++ galerijaslika.sql
@@ -2,10 +2,23 @@ drop database if exists galerija;
 create database galerija;
 use galerija;
 
-create table fotografije(
+create table lokacija(
+sifra int not null primary key auto_increment,
+naziv varchar (50) not null
+);
+
+
+create table fotografija(
     sifra       int not null primary key auto_increment,
     naziv       varchar(50) not null,
     opis        text,
-    lokacija    varchar(50) not null,
+    lokacija    int not null,
     datum datetime
 );
+
+
+alter table fotografija add foreign key (lokacija) references lokacija(sifra);
+
+insert into lokacija (naziv) values ('datotekaprva');
+
+insert into fotografija (naziv, lokacija, datum) values ('naziv.jpg', 1, '2020-05-12');
